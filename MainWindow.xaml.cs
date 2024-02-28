7321010279628ebfb425652261b58509559ecedb
Umid <umidrza4@gmail.com>
2024-02-28 15:42:44 +0400

    Todo App

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Todo_App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = todoInput.Text;
+
+            if (!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock todoItem = new TextBlock
+                {
+                    Text = todoText,
+                    Margin = new Thickness(10),
+                    Foreground = new SolidColorBrush(Colors.White),
+                };
+
+                todoList.Children.Add(todoItem);
+
+                todoInput.Clear();
+            }
+        }
+    }
+}
